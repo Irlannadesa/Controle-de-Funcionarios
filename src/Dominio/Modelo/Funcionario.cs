47a82f454e4ea2107e8fc537320731dda5e7b945
Irlanna de Sá <104098379+Irlannadesa@users.noreply.github.com>
2023-05-30 14:37:49 -0300

    Feat: Criando metodos da API

--- src/Dominio/Modelo/Funcionario.cs
+++ src/Dominio/Modelo/Funcionario.cs
@@ -5,23 +5,13 @@ namespace Dominio.Modelo
     public class Funcionario
     {
         [PrimaryKey, Identity]
-        public int Id { get; set; }
-
-        [Column("Nome"), NotNull]
-        public string Nome { get; set; }
-
-        [Column("Endereco"), NotNull]
+        public int Id { get; set; }        
+        public string Nome { get; set; }        
         public string Endereco { get; set; }
-
-        [Column("CPF"), NotNull]
-        public string CPF { get; set; }
-
-        [Column("Telefone"), NotNull]
+        public string CPF { get; set; }       
         public string Telefone { get; set; }
-
         [Column("Data_Nascimento"), NotNull]
         public DateTime DataNascimento { get; set; }
-
         [Column("Data_Admissao"), NotNull]
         public DateTime DataAdmissao { get; set; }
       
---
+++ src/WebApp/Controllers/FuncionarioController.cs
@@ -0,0 +1,117 @@
+﻿using Dominio.Modelo;
+using Dominio.Validacoes;
+using Infraestrutura.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+        private IFuncionarios _funcionario;
+        //private ValidacoesFuncionario<Funcionario> _validacao;
+
+        public FuncionarioController(IFuncionarios funcionario)
+        {
+            _funcionario = funcionario;
+            //_validacao = validacao;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult ObterTodos()
+        {
+            try
+            {
+                List<Funcionario> funcionario = _funcionario.ObterTodos();
+
+                return Ok(funcionario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            
+            }
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult Criar(Funcionario novoFuncionario)
+        {           
+            if (novoFuncionario == null)
+            {
+                return BadRequest("mensagem de erro - teste");
+            }
+            try
+            {
+                ValidacoesFuncionarios.ValidarCampos(novoFuncionario);
+                _funcionario.Criar(novoFuncionario);
+
+                return Created($"{novoFuncionario.Id}", novoFuncionario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult ObterPorId([FromRoute] int id)
+        {
+            try
+            {
+                Funcionario funcionario = _funcionario.ObterPorId(id);
+
+                return Ok(funcionario);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message + ", \n" + ex.InnerException);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Remover([FromRoute] int id)
+        {
+            try
+            {
+                var funcionario = _funcionario.ObterPorId(id);
+
+                _funcionario.Remover(funcionario.Id);
+
+                return Ok(funcionario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message + ", \n" + ex.InnerException);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Atualizar([FromRoute] int id, [FromBody] Funcionario funcionario)
+        {
+            try
+            {
+                ValidacoesFuncionarios.ValidarCampos(funcionario);
+                _funcionario.Atualizar(funcionario);
+
+                return Ok(funcionario.Id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
