3d5d35798c653c96109e84fe7431a95ae4a232b5
Irlanna de Sá <104098379+Irlannadesa@users.noreply.github.com>
2023-06-12 14:05:09 -0300

    feat: adicionando paginas de detalhes

--- src/WebApp/wwwroot/Controller/ListView.controller.js
+++ src/WebApp/wwwroot/Controller/ListView.controller.js
@@ -1,7 +1,10 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/model/json/JSONModel"
-], function(Controller, JSONModel) {
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator",
+  "sap/ui/core/routing/Router",
+], function(Controller, JSONModel, Filter, FilterOperator, Router) {
   "use strict";
   return Controller.extend("controleDeFuncionarios.Controller.ListView", {
     onInit: function() {
@@ -14,9 +17,29 @@ sap.ui.define([
         })
         .catch((error) => {
           console.error(error);
-        });
-
+        });        
         
-    }
+    },
+
+
+    barraPesquisa: function (oEvent) {
+      let filtro = [];
+      let buscar = oEvent.getParameter("query");
+      if (buscar) {
+        filtro.push(new Filter("nome", FilterOperator.Contains, buscar));
+      }
+    
+      let lista = this.getView().byId("myList");
+      let items = lista.getBinding("items");
+      items.filter(filtro);
+    },
+
+    aoClicarNaLinha: function (oEvent) {
+      var oItem = oEvent.getSource();
+      let rota = this.getOwnerComponent().getRouter();
+      let idDaLinhaSelecionada = oEvent.getSource().getBindingContext().getProperty("id")
+      rota.navTo("details", { id: idDaLinhaSelecionada })
+  }   
+  
   });
 });
